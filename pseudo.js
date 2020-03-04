fddbffbb1cec5de86398e34999eb4bf82f45b40a
Tom Clayton <tgclayton1@gmail.com>
2020-03-05 06:18:14 +1300

    setup calculator

---
+++ pseudo.js
@@ -0,0 +1,7 @@
+//overall process is to recieve inputs based on a standard calculator setup and convert them into a computable string whose result is then displayed
+// while displaying the last number pressed on the screen
+
+
+//creats a variable to store the entire input before presseing equal
+// when a number is clicked, it is added to the temp variable
+// 
